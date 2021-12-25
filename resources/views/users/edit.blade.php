0ae55c05b7b20b1e57b7b2ffb602e8aec64684ee
Hassan Wael <hassanwael1999@gmail.com>
2021-12-25 12:16:04 +0200

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', ['page' => __('manage_user'), 'pageSlug' => 'manage_user'])
 @section('content')
-<div class="col-lg-6 col-md-6 col-sm-12 width">
+{{-- <div class="col-lg-6 col-md-6 col-sm-12 width">
     <div class="address">
         <h3>Update</h3>
         <form method="post" action='{{route('update_user',$user)}}'>
@@ -20,6 +20,52 @@
             </div>
         </form>
     </div>
-</div>
+</div> --}}
+<div class="row">
+    <div class="col-md-12">
+        <form method="get" action="{{Route('update_user')}}" autocomplete="off">
+            <div class="card-body">
+                    @csrf
+                    @include('alerts.success')
 
+                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                        <label>{{ __('الاسم') }}</label>
+                        <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{$user->name }}">
+                        @include('alerts.feedback', ['field' => 'name'])
+                    </div>
+
+                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                        <label>{{ __('البريد الالكتروني') }}</label>
+                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email address') }}" value="{{$user->email }}">
+                        @include('alerts.feedback', ['field' => 'email'])
+                    </div>
+                    <div class="form-group">
+                        <label>{{ __('كلمة السر') }}</label>
+
+                    <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <i class="tim-icons icon-lock-circle"></i>
+                            </div>
+                        </div>
+                        <input type="password" value="{{$user->password}}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('كلمة السر ') }}">
+                        @include('alerts.feedback', ['field' => 'password'])
+                    </div>
+                </div>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <i class="tim-icons icon-lock-circle"></i>
+                            </div>
+                        </div>
+                        <input type="password" value="{{$user->password}}" name="password_confirmation" class="form-control" placeholder="{{ __('تأكيد كلمة السر ') }}">
+                    </div>
+            </div>
+            <div class="card-footer">
+                <input type="submit" class="btn btn-fill btn-primary"></input>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
